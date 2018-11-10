939ef498e88bd41db7870ceeb9977367aac66ee7
Steve <ssmock@gmail.com>
2018-11-10 00:02:47 -0500

    Add a proper entry point

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import Clock from "./clock";
+declare const _default: {
+    Clock: typeof Clock;
+};
+export default _default;
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var clock_1 = __importDefault(require("./clock"));
+exports.default = {
+    Clock: clock_1.default
+};
