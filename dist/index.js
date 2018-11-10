bcc5ef407b7baa5d42011b462c1cd7612432dcfe
Steve <ssmock@gmail.com>
2018-11-10 00:58:00 -0500

    Yes

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-var clock_1 = require("./clock");
-exports.Clock = clock_1.Clock;
+__export(require("./clock"));
